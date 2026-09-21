f8914f71a0ff2d3a8e5497a81f5f4c5d38acf549
Gerald ATB <gtumbaco074@boscdelacoma.cat>
2026-09-21 18:57:59 +0200

    get tracks

--- entorn-servidor/mini-api/src/index.ts
+++ entorn-servidor/mini-api/src/index.ts
@@ -6,15 +6,25 @@ import { Track } from "./interfaces/data/track.js";
 const app: Express = express();
 
 app.get("/", (_req: Request, res: Response) => {
-    return res.json(APICONFIG);
+    return res.json(JSON.stringify(APICONFIG));
+});
+
+app.get("/tracks", (_req: Request, res: Response) => {
+    return res.status(200).json(tracks);
 });
 
 app.get("/tracks/:id", (req: Request, res: Response) => {
     const idTrack: string = req.params.id as string;
-    const track: Track[] = tracks.filter((t: Track) => t.id === idTrack);
+
+    const track: Track[] = tracks.filter(
+        (t: Track) => { return t.id === idTrack }
+    );
+    if (track.length === 0) {
+        return res.status(404).json({ message: `Track ${idTrack} not found` })
+    }
     return res.status(200).json(track);
 });
 
 app.listen(APICONFIG.port, APICONFIG.host, () => {
     console.log(`Servidor escoltant a http://${APICONFIG.host}:${APICONFIG.port}`);
-});
+})
