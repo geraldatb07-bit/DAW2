ce8d8c6fe2056be7e34f6a590c755f54f6a2fe90
Gerald ATB <gtumbaco074@boscdelacoma.cat>
2026-09-25 16:36:35 +0200

    Alinea les durades de les pistes
    Converteix les durades inicials al format numèric definit per la interfície Track.

--- entorn-servidor/spotify-api/src/data/track/track.ts
+++ entorn-servidor/spotify-api/src/data/track/track.ts
@@ -5,18 +5,12 @@ export const tracks: Track[] = [
         id: "4395-abd1-cd31-rf90",
         title: "Nights like this",
         artist: "KidLaroi",
-        duration: {
-            minutes: 2,
-            seconds: 45
-        }
+        duration: 165
     },
     {
         id: "9801-aqd1-xd11-rq55",
         title: "Rattle and hum",
         artist: "U2",
-        duration: {
-            minutes: 3,
-            seconds: 10
-        }
+        duration: 190
     }
 ];
